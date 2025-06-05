41ca8ff8f3d87d61e47011f0564b4257795f43f1
Vishesh <vishesh98sharma@gmail.com>
2025-06-04 20:39:45 -0400

    Deployment

--- ecom-frontend/src/api/api.js
+++ ecom-frontend/src/api/api.js
@@ -4,5 +4,22 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:8080/api",
   withCredentials: true,
 });
+api.interceptors.request.use(
+  (config) => {
+    const authData = localStorage.getItem("auth");
+    if (authData) {
+      const token = JSON.parse(authData).jwtToken || JSON.parse(authData).token;
+      console.log("Attaching token:", token);
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } else {
+      console.log("No auth data found");
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 
 export default api;
